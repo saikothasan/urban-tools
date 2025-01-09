2ea57d6a4f7ee97f5640461f3d230c95b655f508
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-01-10 03:50:45 +0600

    Create react-json-editor-ajrm.d.ts

---
+++ types/react-json-editor-ajrm.d.ts
@@ -0,0 +1,17 @@
+declare module 'react-json-editor-ajrm/locale/en' {
+  const locale: {
+    format: string;
+    oops: string;
+    tryAgain: string;
+    copy: string;
+    copyToClipboard: string;
+    paste: string;
+    pasteFromClipboard: string;
+    delete: string;
+    insert: string;
+    properties: string;
+    elements: string;
+  };
+  export default locale;
+}
+
